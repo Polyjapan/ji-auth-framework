e1fb6313046a989c33eca232250aadc558067d04
zyuiop <zyuiop@zyuiop.net>
2019-05-03 12:51:33 +0200

    Add a isValidLogin flag on ticket types

--- src/main/scala/ch/japanimpact/auth/api/TicketType.scala
+++ src/main/scala/ch/japanimpact/auth/api/TicketType.scala
@@ -6,7 +6,12 @@ import play.api.libs.json._
   * @author Louis Vialar
   */
 
-trait TicketType
+trait TicketType {
+  /**
+    * A boolean indicating whether this ticket represents a successful login and can be used to authorize an user
+    */
+  val isValidLogin: Boolean = false
+}
 
 object TicketType {
   def apply(s: String): TicketType = s match {
@@ -32,7 +37,9 @@ object TicketType {
   /**
     * A ticket emitted when the user successfully logged in using its email and password
     */
-  case object LoginTicket extends TicketType
+  case object LoginTicket extends TicketType {
+    override val isValidLogin: Boolean = true
+  }
 
   /**
     * A ticket emitted when the user successfully registered against the system but did not confirm its email yet
@@ -47,23 +54,31 @@ object TicketType {
   /**
     * A ticket emitted when the user just confirmed his email address
     */
-  case object EmailConfirmTicket extends TicketType
+  case object EmailConfirmTicket extends TicketType {
+    override val isValidLogin: Boolean = true
+  }
 
   /**
     * A ticket emitted when the user just reset his password using a previously sent link
     */
-  case object PasswordResetTicket extends TicketType
+  case object PasswordResetTicket extends TicketType {
+    override val isValidLogin: Boolean = true
+  }
 
   /**
     * A ticket emitted when the user is sent to an app using the explicit mode
     */
-  case object ExplicitGrantTicket extends TicketType
+  case object ExplicitGrantTicket extends TicketType {
+    override val isValidLogin: Boolean = true
+  }
 
   /**
     * A ticket emitted when an app requests a ticket for itself. It means the caller is not an
     * user but an app.
     */
-  case object AppTicket extends TicketType
+  case object AppTicket extends TicketType {
+    override val isValidLogin: Boolean = true
+  }
 
   implicit val sourceFormat: Format[TicketType] = new Format[TicketType] {
     override def reads(json: JsValue): JsResult[TicketType] = json match {
